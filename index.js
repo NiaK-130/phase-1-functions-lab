b8803191a5eff63325f7fee29dc9300f556bd538
Nia K <nsjkappen@gmail.com>
2021-07-23 13:27:57 -0700

    commit tree 52176c45bf1b9e7cb1f4e13db1afd06e37c80803

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-// Code your solution in this file!
+// // Code your solution in this file!
 
-const headquaters = 42;
+// const headquaters = 42;
 
-function distanceFromHqInBlocks(dist){
+// function distanceFromHqInBlocks(dist){
     
-    var distance = Math.abs(headquaters - dist);
+//     var distance = Math.abs(headquaters - dist);
 
-    return distance
+//     return distance
   
-}
+// }
 
-//--------------------------------------------------------//
+// //--------------------------------------------------------//
 
-function distanceFromHqInFeet(feet){
+// function distanceFromHqInFeet(feet){
 
-    var block = 264;
-    var distanceinfeet = distanceFromHqInBlocks(feet)*block;
+//     var block = 264;
+//     var distanceinfeet = distanceFromHqInBlocks(feet)*block;
 
-    return distanceinfeet
+//     return distanceinfeet
 
-}
+// }
 
-//--------------------------------------------------------//
-
-
-function  distanceTravelledInFeet(start,end){
-
-    var block = 264
-
-    var total = Math.abs(end - start)*block
-
-    return total 
-
-}
+// //--------------------------------------------------------//
 
 
-//--------------------------------------------------------//
+// function  distanceTravelledInFeet(start,end){
+
+//     var block = 264
+
+//     var total = Math.abs(end - start)*block
+
+//     return total 
+
+// }
 
 
-function calculatesFarePrice(start, destination){
+// //--------------------------------------------------------//
 
-    var output = distanceTravelledInFeet(start,destination);
-    var output1 = output - 400;
-    var cost = 0.02;
 
-    if (output <= 400){
-        return 0;
-    } else if (output > 400 && output <= 2000){
-        return output1*cost;
-    } else if (output > 2000 && output < 2500){
-        return 25
-    } else if (output >= 2500){
-        return 'cannot travel that far';
-    }
+// function calculatesFarePrice(start, destination){
 
-}
+//     var output = distanceTravelledInFeet(start,destination);
+//     var output1 = output - 400;
+//     var cost = 0.02;
+
+//     if (output <= 400){
+//         return 0;
+//     } else if (output > 400 && output <= 2000){
+//         return output1*cost;
+//     } else if (output > 2000 && output < 2500){
+//         return 25
+//     } else if (output >= 2500){
+//         return 'cannot travel that far';
+//     }
+
+// }
+
